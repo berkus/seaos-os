d21c701b85b4c053cd0ad009de68ae64842453d6
goldenpiranha <goldenpiranha@d26e93ba-32f4-56fc-da15-f0b46964556f>
2012-05-09 05:08:56 +0000

    fixed environ setting

--- trunk/data/t.c
+++ trunk/data/t.c
@@ -3,8 +3,10 @@
 #include <string.h>
 #include <math.h>
 extern void test_function();
-int main(int c, char **a, char **ee)
+int main(int c, char **a)
 {
+	char *path = getenv("testvar");
+	printf(">> %s <<\n", path);
 	printf("You can't fight in here! This is the war room!\n");
 	return 0;
 }
