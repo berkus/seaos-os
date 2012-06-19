d902ff1a7ad734c1e179980bf808bd3fdd89d638
goldenpiranha <goldenpiranha@d26e93ba-32f4-56fc-da15-f0b46964556f>
2012-06-19 21:26:17 +0000

    added man script

--- trunk/tools/chd.sh
+++ trunk/tools/chd.sh
@@ -28,16 +28,14 @@ echo processing hd2.img..
 dd if=/dev/zero of=hd2.img bs=1024 count=1 2> /dev/null
 dd if=/dev/zero of=hd2.img bs=1024 count=1 seek=100000 2> /dev/null
 yes | (sudo /sbin/mke2fs -q -I128 -b1024 hd2.img) > /dev/null
-echo copying data/ to hd.img...
 sh ./tools/open_hdimage.sh
-sudo cp -f -r data/* /mnt-seaos/
-sudo cp -f -r doc/man/* /mnt-seaos/usr/man/
 echo copying apps/data/ to hd.img...
 sudo cp -f -r apps/data/* /mnt-seaos/
 sudo rm -rf `sudo find /mnt-seaos -name .svn`
 sudo rm -f `sudo find /mnt-seaos -name .directory`
 sudo rm -rf `sudo find /mnt-seaos -name man`
-echo copying documentation to hd.img...
+echo copying data/ to hd.img...
+sudo cp -f -r data/* /mnt-seaos/
 sudo mkdir -p /mnt-seaos/usr/man
 sudo cp -rf tools/man_gen_tmp/text/* /mnt-seaos/usr/man/
 sh ./tools/close_hdimage.sh
