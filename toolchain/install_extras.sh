3215addff9d76aaf8fa617e6cbd70851bcab1d2d
goldenpiranha <goldenpiranha@d26e93ba-32f4-56fc-da15-f0b46964556f>
2012-10-21 20:35:09 +0000

    toolchain and build system no longer need sudo

--- toolchain/install_extras.sh
+++ toolchain/install_extras.sh
@@ -6,29 +6,29 @@ tar xjf extra-libs.tar.bz2
 cd termcap-1.3.1
 ./configure --host=i586-pc-seaos --prefix=$INSTALL_LOC/i586-pc-seaos
 make CC=i586-pc-seaos-gcc AR=i586-pc-seaos-ar RANLIB=i586-pc-seaos-ranlib
-sudo make install
+make install
 
 cd ../ncurses-5.9
 ./configure --host=i586-pc-seaos --enable-termcap --disable-database --prefix=$INSTALL_LOC/i586-pc-seaos/ --without-cxx --without-cxx-binding --oldincludedir=$INSTALL_LOC/i586-pc-seaos/include --includedir=$INSTALL_LOC/i586-pc-seaos/include
 make
-sudo make install
+make install
 
 cd ../readline-6.2
 ./configure --host=i586-pc-seaos --prefix=$INSTALL_LOC/i586-pc-seaos --without-curses --disable-shared
 make
-sudo make install
+make install
 
 cd ../gmp-5.0.4
 ./configure --host=i586-pc-seaos --prefix=$INSTALL_LOC/i586-pc-seaos
 make
-sudo make install
+make install
 
 cd ../mpfr-3.1.0
 ./configure --host=i586-pc-seaos --prefix=$INSTALL_LOC/i586-pc-seaos CFLAGS='-fno-stack-protector'
 make CC=i586-pc-seaos-gcc AR=i586-pc-seaos-ar RANLIB=i586-pc-seaos-ranlib CFLAGS='-fno-stack-protector'
-sudo make install
+make install
 
 cd ../mpc-0.9
 ./configure --host=i586-pc-seaos --prefix=$INSTALL_LOC/i586-pc-seaos
 make
-sudo make install
+make install
